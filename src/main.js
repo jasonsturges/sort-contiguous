1bef39b7cb543202189e23bdf2cc53fc48eb869c
Jason Sturges <jason@jasonsturges.com>
2021-02-17 17:04:14 -0600

    Initial implementation

--- src/main.js
+++ src/main.js
@@ -1,12 +1,47 @@
-import ms from 'ms';
-import lunchtime from './lunchtime.js';
-import millisecondsUntil from './millisecondsUntil.js';
+/**
+ * Prefix regular expression
+ * @type {RegExp}
+ */
+const regPrefix = /\D/;
 
-export default function howLongUntilLunch(hours, minutes) {
-	// lunch is at 12.30
-	if (hours === undefined) hours = 12;
-	if (minutes === undefined) minutes = 30;
+/**
+ * Suffix regular expression
+ * @type {RegExp}
+ */
+const regSuffix = /\d+$/;
 
-	var millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
-	return ms(millisecondsUntilLunchTime, { long: true });
+/**
+ * Sort comparer function for contiguous prefix.
+ */
+export const contiguousPrefixComparer = (a, b) => {
+  let x = regPrefix.exec(a);
+  let y = regPrefix.exec(b);
+
+  return a.slice(x.index).localeCompare(b.slice(y.index)) ||
+         a.slice(0, x.index) - b.slice(0, y.index);
+}
+
+/**
+ * Sort comparer function for contiguous suffix.
+ */
+export const contiguousSuffixComparer = (a, b) => {
+  let x = regSuffix.exec(a);
+  let y = regSuffix.exec(b);
+
+  return a.slice(0, x.index).localeCompare(b.slice(0, y.index)) ||
+         a.slice(x.index) - b.slice(y.index);
+}
+
+/**
+ * @param {String[]} list - List to be sorted contiguously.
+ */
+export const sortContiguousPrefix = (list) => {
+  return list.sort(contiguousPrefixComparer);
+}
+
+/**
+ * @param {String[]} list - List to be sorted contiguously.
+ */
+export const sortContiguousSuffix = (list) => {
+  return list.sort(contiguousSuffixComparer);
 }
